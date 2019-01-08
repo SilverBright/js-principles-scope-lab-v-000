1e9a5ed7338f37722b48ccf3e7b17946817cc989
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-08 21:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your solution in this file!
 
 var customerName = 'bob'
-  //return customerName
 
-//function upperCaseCustomerName(customerName)
+function upperCaseCustomerName(customerName) 
