4d2a5260df83a270acf3268b5960384b7c662753
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-08 21:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your solution in this file!
 
 var customerName = 'bob';
+var bestCustomer = 'not bob'
 
 function upperCaseCustomerName () {
   return customerName = customerName.toUpperCase()
